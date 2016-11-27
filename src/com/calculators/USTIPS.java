b356bdc05b079105e847463a5af13188c1039742
skhansfdc <sameer15@hotmail.com>
2016-11-26 21:40:39 -0600

    Add existing file

---
+++ src/com/calculators/USTIPS.java
@@ -0,0 +1,46 @@
+package com.calculators;
+
+public class USTIPS 
+{
+	private class I
+	{
+		
+	}
+	public static void main(String args[])
+	{
+		//Inputs: 1) bond coupon, 2) bond time to maturity (in months)
+			//	  3) expected yearly inflation rate, 4) total amount paid for the purchase
+		    //    5) bond(s) amount
+		//Outputs: 1) Return on premium (Input# 4 - Input# 5) as percentage, 2) Actual $$ value of the premium investment
+		    //     3) Inflation adjusted value of the premium investment, given Input# 3 
+		
+		//Configure Inputs
+		double bondCoupon = 1.18737;
+		int timeToMaturityInMonths = 126;
+		double inflationRate = 1;
+		double bondPurchaseAmount = 29864;
+		double bondParVal = 20000;
+		
+		double couponInterestEarning = 0;
+		double inflationAdjustedParVal = bondParVal;
+		double paidPremiumAmount = bondPurchaseAmount - bondParVal;
+		double inflationAdjustedPaidPremiumVal = paidPremiumAmount;
+		
+		for(int a = 0 ; a < timeToMaturityInMonths ; a++)
+		{
+			inflationAdjustedPaidPremiumVal = inflationAdjustedPaidPremiumVal + (inflationAdjustedPaidPremiumVal * ((inflationRate/12)/100));
+			inflationAdjustedParVal = inflationAdjustedParVal + (inflationAdjustedParVal * ((inflationRate/12)/100));
+			couponInterestEarning = couponInterestEarning + (inflationAdjustedParVal * ((bondCoupon/12)/100));
+		}
+		
+		couponInterestEarning = Math.round(couponInterestEarning);
+		inflationAdjustedParVal = Math.round(inflationAdjustedParVal);
+		inflationAdjustedPaidPremiumVal = Math.round(inflationAdjustedPaidPremiumVal);
+		double returnOnPremiumPaid = Math.round(((couponInterestEarning - inflationAdjustedPaidPremiumVal) / inflationAdjustedPaidPremiumVal)*100);
+		
+		System.out.println("[Actual $$ val of the amount recouped towards premium] "+couponInterestEarning);
+		System.out.println("[Inflation adjusted par value] "+inflationAdjustedParVal);
+		System.out.println("[Inflation adjusted premium value] "+inflationAdjustedPaidPremiumVal);
+		System.out.println("[Return on paid premium] "+returnOnPremiumPaid);
+	}
+}
